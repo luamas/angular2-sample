cbf9d22a1a7547229600452b0d4417d16a456f70
stategrace <stategrace@163.com>
2017-03-22 13:18:22 +0800

    first

---
+++ src/app/skill.ts
@@ -0,0 +1,14 @@
+/**
+ *
+ * User: luamas
+ * DateTime: 2017/3/22 12:27
+ */
+
+export class Skill {
+  id: number;
+  name: string;
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
